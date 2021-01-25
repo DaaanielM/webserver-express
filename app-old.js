394f2bb5938290090d49312ab2b9421e5038d9e2
DanielM <danieljhonsend99@gmail.com>
2021-01-25 11:15:28 -0500

    Primer commit

---
+++ app-old.js
@@ -0,0 +1,21 @@
+// EL HTTP es un paquete que nos permite crear rapidamente un web server
+
+
+const http = require('http');
+
+http.createServer((req,res) => {
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+        let salida = {
+            nombre: 'Daniel',
+            edad: 21,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        res.end();
+})
+
+.listen(8080);
+
+console.log('Escuchando el puerto 8080');
